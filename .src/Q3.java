0e78bbd6dc02c6f5b1e68655ddb94149a12ae812
zheyu <zheyunge@gmail.com>
2025-11-16 19:52:02 +0800

    Clean file and fix bug

--- .src/Q3.java
+++ .src/Q3.java
@@ -2,7 +2,7 @@ package lab.viva1;
 import java.util.Scanner;
 
 
-public class Question3 {
+public class Q3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
